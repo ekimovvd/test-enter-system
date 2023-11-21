26257e74602ae99d4a2bad5e2b1db2397e491ceb
ekimov_vd <ekimov_vd@mail.ru>
2023-11-21 03:04:09 +0300

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>test enter system</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
